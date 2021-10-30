27159e15c57e5707bcafeca8ad5bc25521b15b0a
Tanishq Gandhi <tangandhi19@gmail.com>
2021-10-30 21:01:38 +0530

    added

---
+++ src/information/cards.js
@@ -0,0 +1,31 @@
+import React from "react";
+import loadingimage from "./loading.gif"
+const Users = ({ loading, users }) => {
+  const mainstyle = {
+    display: "flex"
+  };
+  return loading ? (
+    <div id="main" align="center">
+      <img
+        src={loadingimage}        alt="loading.."
+        className="loader"
+      />
+    </div>
+  ) : (
+    <div id="main" style={mainstyle}>
+      {users.map((user) => (
+        <div className="profile_cont" key={user.id}>
+          {console.log(user)}
+
+          <img src={user.avatar} alt={user.avatar} className="avatar"></img>
+          <h1 className="name">
+            {user.first_name} {user.last_name}
+          </h1>
+          <p className="email">{user.email}</p>
+          <p>User id: {user.id}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+export default Users;
